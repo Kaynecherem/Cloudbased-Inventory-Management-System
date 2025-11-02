32c3ae1ffe6cb0f536e3d6a77dd392d65bc74d2d
kaynecherem <kaluchinecherem8@gmail.com>
2025-11-02 02:27:48 +0100

    first commit

---
+++ src/main/java/hr/algebra/cloudbased_inventory_management_system/CloudbasedInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.cloudbased_inventory_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbasedInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbasedInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/algebra/cloudbased_inventory_management_system/CloudbasedInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.cloudbased_inventory_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbasedInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
